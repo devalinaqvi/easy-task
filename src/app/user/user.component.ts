75aa597f2191a8a9a42e573d1dac9d03825f749b
Ali Naqvi <dev.alinaqvi@gmail.com>
2025-06-07 08:31:19 +0500

    Get a random index from users array

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS} from "./dummy-users";
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
