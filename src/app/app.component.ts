ee7e0c16233a3ff3aad13a57286c29e1cadc6f67
devalinaqvi <dev.alinaqvi@gmail.com>
2025-08-19 12:20:38 +0500

    Add DUMMY_USERS import and initialize users in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import {UserComponent} from "./user/user.component";
+import { DUMMY_USERS } from "./user/dummy-users";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,5 @@ import {UserComponent} from "./user/user.component";
 })
 export class AppComponent {
   title = 'my-first-ng-project';
+  users = DUMMY_USERS;
 }
